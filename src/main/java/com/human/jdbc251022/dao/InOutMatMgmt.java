86b5e6b17a0c89423048489b7ea0a8dea411a432
LSM <qdpnok@naver.com>
2025-10-23 16:37:59 +0900

    겹치는 부분 삭제

--- src/main/java/com/human/jdbc251022/dao/InOutMatMgmt.java
+++ src/main/java/com/human/jdbc251022/dao/InOutMatMgmt.java
@@ -15,4 +15,28 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class InOutMatMgmt {
+    private final JdbcTemplate jdbcTemplate;
+
+    // 회원 전체 조회
+    public List<Employee> empList() {
+        String query = "SELECT * FROM member";
+        return jdbcTemplate.query(query, new MgmtDao.EmpRowMapper());
+    }
+
+    private static class EmpRowMapper implements RowMapper<Employee> {
+
+        // ResultSet -> DB에서 넘어온 결과, rowNum -> 행 번호
+        // 행 번호로 자동으로 돌아서 Member에 넣어준다
+        @Override
+        public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Employee(
+                    rs.getInt("emp_id"),
+                    rs.getString("emp_name"),
+                    rs.getString("job"),
+                    rs.getInt("dept_id"),
+                    rs.getInt("sal"),
+                    rs.getTimestamp("hiredate").toLocalDateTime()
+            );
+        }
+    }
 }
---
+++ src/main/java/com/human/jdbc251022/model/Inbound.java
@@ -0,0 +1,9 @@
+package com.human.jdbc251022.model;
+
+public class Inbound {
+    private int id;
+    private int matId;
+    private int empId;
+    private int qty;
+
+}
